a45d34d55ac40b02f20a0de3cc26451cdbc16dd5
Konrad <00ksiuzdak00@gmail.com>
2020-03-03 08:08:41 +0100

    Transposer finished.

--- src/Song.java
+++ src/Song.java
@@ -1,5 +1,6 @@
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -7,13 +8,11 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-
-import java.util.Observable;
 
 public class Song extends TreeNode {
     StringProperty text;
+    StringProperty chords;
+    private final static Transposer transposer=new Transposer();
 
     public Song(int number,String title, String text) {
         super(number);
@@ -21,6 +20,8 @@ public class Song extends TreeNode {
         this.text.setValue(text);
         this.title= new SimpleStringProperty();
         this.title.setValue(title);
+        this.chords=new SimpleStringProperty();
+        this.chords.setValue("");
     }
 
     @Override
@@ -38,44 +39,84 @@ public class Song extends TreeNode {
 
         //GENERAL
         BorderPane result=new BorderPane();
+
         result.getStylesheets().add("Style.css");
         result.getStyleClass().add("songPresentation");
 
 
         //TITLE HBOX
         HBox titleHBox=new HBox();
-        Text title=new Text(this.title.getValue());
+        TextArea titleTextArea=new TextArea(this.title.getValue());
+        titleTextArea.setPrefHeight(100);
+        titleTextArea.setWrapText(true);
+        titleTextArea.getStyleClass().add("titleTextArea");
+        //Text title=new Text(this.title.getValue());
         titleHBox.getStyleClass().add("titleHBox");
-        titleHBox.getChildren().add(title);
+        titleHBox.getChildren().add(titleTextArea);
         titleHBox.setAlignment(Pos.CENTER);
         result.setTop(titleHBox);
+
+
+        //CHORDS
+        VBox chordsVBox=new VBox();
+        TextArea chordsTextArea=new TextArea();
+        chordsTextArea.setText(this.chords.getValue());
+        chordsTextArea.setWrapText(true);
+        chordsVBox.getChildren().add(chordsTextArea);
 
         //TEXT VBOX
         VBox textVBox=new VBox();
         TextArea textArea=new TextArea(this.text.getValue());
+        textArea.setWrapText(true);
+        textArea.setPrefHeight(500);
         //Text text=new Text(this.text.getValue());
         textVBox.getStyleClass().add("textVBox");
         //textVBox.getChildren().add(text);
-        textVBox.getChildren().add(textArea);
+        textVBox.getChildren().addAll(chordsTextArea,textArea);
         result.setCenter(textVBox);
         textVBox.setAlignment(Pos.TOP_CENTER);
 
+
+
+
         //BUTTONS
+            //LEFT
         VBox left=new VBox();
-        left.setSpacing(10);
-        Button prev=new Button("Previous");
+        left.setSpacing(100);
+        Button prev=new Button("Back");
         prev.getStyleClass().add("button");
         Button listenToTempo=new Button("Tempo");
         listenToTempo.getStyleClass().add("button");
         left.getChildren().addAll(prev,listenToTempo);
         result.setLeft(left);
-
+            //RIGHT
         VBox right=new VBox();
-        right.setSpacing(10);
-        Button next=new Button("next");
+        right.setSpacing(20);
+        Button transUpButton=new Button("+1");
+        Button transDownButton=new Button("-1");
+        transUpButton.setOnAction(e-> {
+            this.chords.setValue(transposer.transpose(this.chords.getValue(),true));
+            chordsTextArea.setText(this.chords.getValue());
+        });
+        transDownButton.setOnAction(e-> {
+            this.chords.setValue(transposer.transpose(this.chords.getValue(),false));
+            chordsTextArea.setText(this.chords.getValue());
+        });
+        Button next=new Button("Next");
+        Button save=new Button("Save");
+        save.getStyleClass().add("button");
+        save.setOnAction(e->{
+            this.text.setValue(textArea.getText());
+            this.title.setValue(titleTextArea.getText());
+            this.chords.setValue(chordsTextArea.getText());
+        });
         next.getStyleClass().add("button");
-        right.getChildren().addAll(next);
+        right.getChildren().addAll(next,save,transUpButton, transDownButton);
         result.setRight(right);
+
+
+        BorderPane.setMargin(right, new Insets(10));
+        BorderPane.setMargin(left, new Insets(10));
         return result;
     }
 }
---
+++ src/Transposer.java
@@ -0,0 +1,88 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Transposer {
+    private final Set<String> possibleChords;
+    private final Map<Character, Integer> nameToNumber;
+    private final char[] numberToName= new char[]{'F', 'C','G','D','A','E','B'};
+
+    public Transposer() {
+        nameToNumber=new HashMap<>();
+        nameToNumber.put('F',0);
+        nameToNumber.put('C',1);
+        nameToNumber.put('G',2);
+        nameToNumber.put('D',3);
+        nameToNumber.put('A',4);
+        nameToNumber.put('E',5);
+        nameToNumber.put('B',6);
+
+        possibleChords=new HashSet<>(Arrays.asList("Cb","C","C#",
+                "Db","D","D#","Eb","E","E#","Fb","F","F#","Gb","G","G#","Ab","A","A#","Bb","B","B#"));
+
+    }
+
+    public String transpose(String chords,boolean up)
+    {
+        StringBuilder buffer=new StringBuilder();
+        int i=0,j=0;
+        while(i<chords.length())
+        {
+            if(chords.charAt(i)!=' ')
+            {
+                j=i;
+                while (j<chords.length()&&chords.charAt(j)!=' ')
+                {
+                    j++;
+                    if(j-i>2)
+                    {
+                        throw new RuntimeException("Bad transpose input1");
+                    }
+                }
+                if(j-i==2&&possibleChords.contains(chords.substring(i,i+1).toUpperCase()))
+                {
+                    buffer.append(this.go(chords.substring(i,j),up));
+                    buffer.append(" ");
+                }
+                else if(j-i<2&&possibleChords.contains(chords.substring(i,j).toUpperCase()))
+                {
+                    buffer.append(this.go(chords.substring(i,j),up));
+                    buffer.append(" ");
+                }
+                else if(j-i>2) throw new RuntimeException("Bad transpose input2");
+
+            }
+            i++;
+        }
+        return buffer.toString();
+    }
+
+    private String go(String chord,boolean up)
+    {
+        int multiplier= up? 1:-1;
+        char[] characters=new char[2];
+        int position=multiplier*7;
+        char c=chord.charAt(0);
+        position+=this.nameToNumber.get(Character.toUpperCase(c));
+        if(chord.length()==2)
+        {
+            if(chord.charAt(1)=='#')
+                position+=7;
+            else if(chord.charAt(1)=='b')
+                position-=7;
+        }
+        else if(chord.length()>2) throw new RuntimeException("too long chords");
+        while(position>=12)
+            position-=12;
+        while(position<0)
+            position+=12;
+
+        characters[0]=Character.isUpperCase(c)?this.numberToName[Math.abs(position)%7]:Character.toLowerCase(this.numberToName[Math.abs(position)%7]);
+        if(position/7==0)
+            characters[1]=' ';
+        else
+            characters[1]='#';
+
+        return new String(characters);
+
+    }
+}
